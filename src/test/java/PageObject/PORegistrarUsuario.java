c0944e2ef706dba215e2db8b83399e5846cee41e
amuniz88 <a.muniz1488@gmail.com>
2019-06-17 21:53:10 -0300

    Primer Guardado - BasePage - HomePage - PORegistrarUsuario - Testcase

---
+++ src/test/java/PageObject/PORegistrarUsuario.java
@@ -0,0 +1,11 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class PORegistrarUsuario extends BasePage{
+
+    public PORegistrarUsuario(WebDriver driver){
+        super(driver);
+    }
+
+}
